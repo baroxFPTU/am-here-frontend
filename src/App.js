3829fa687b2a082721825c786f90c8c6538b5fd0
Bao Minh <haphanabominh9674@gmail.com>
2022-10-15 11:45:49 +0700

    demo push

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthLayout from './components/layout/AuthLayout';
-import MainLayout from './components/layout/MainLayout';
-import PrivateRoute from './components/routes/PrivateRoute';
-import PublicRoute from './components/routes/PublicRoute';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import Chat from "pages/Chat";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthLayout from "./components/layout/AuthLayout";
+import MainLayout from "./components/layout/MainLayout";
+import PrivateRoute from "./components/routes/PrivateRoute";
+import PublicRoute from "./components/routes/PublicRoute";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<MainLayout />}>
-            <Route path='' element={<Home />} />
+          <Route path="/" element={<MainLayout />}>
+            <Route path="" element={<Home />} />
           </Route>
-          <Route path='*' element={<NotFound />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
