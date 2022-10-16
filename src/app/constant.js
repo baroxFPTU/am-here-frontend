4e4307e401d48af9940fb367be836915af5c9910
baroxFPTU <baopqse160018@fpt.edu.vn>
2022-10-16 12:37:15 +0700

    fix buggggg

--- src/app/constant.js
+++ src/app/constant.js
@@ -1,3 +1,3 @@
 export const ROLE_MEMBER_STRING = 'member';
 export const ROLE_LISTENER_STRING = 'listener';
-export const REACT_APP_API_URL = 'https://3d2d-118-69-233-165.ap.ngrok.io/api';
+export const REACT_APP_API_URL = 'https://005e-118-69-233-165.ap.ngrok.io/api';
